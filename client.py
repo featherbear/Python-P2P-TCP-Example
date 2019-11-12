9b890be963f225d9e4662557bbd36fe5198b2fe9
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-12 20:01:04 +1100

    * attempt to make things easier to understand *

--- client.py
+++ client.py
@@ -3,40 +3,40 @@ SERVER_PORT = 1234
 
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+clientToRelayServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientToRelayServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 print(f"Connecting to RELAY SERVER @ {SERVER_HOST}:{SERVER_PORT}...\n")
-client.connect((SERVER_HOST, SERVER_PORT))
+clientToRelayServer.connect((SERVER_HOST, SERVER_PORT))
 
-print("> Local connection to RELAY SERVER:", client.getsockname())
+print("> Local connection to RELAY SERVER:", clientToRelayServer.getsockname())
 
-myServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-myServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-myServer.bind(client.getsockname())
-myServer.listen()
-assert client.getsockname() == myServer.getsockname()
-print("> Starting P2P server on", myServer.getsockname())
+portThatClientUsedToConnectToMainServer = clientToRelayServer.getsockname()[1]
+peerServerListeningAddress = ("0.0.0.0", portThatClientUsedToConnectToMainServer)
+myPeerServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+myPeerServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+print("> Starting P2P server on", peerServerListeningAddress)
+myPeerServer.bind(peerServerListeningAddress)
+myPeerServer.listen()
 
 import select
 import time
 
-readList = [client, myServer]
+readList = [clientToRelayServer, myPeerServer]
 
 while True:
   conns, _, _ = select.select(readList, [], [], 1)
   for connection in conns:
-    if connection is myServer:
-      newConnection, client_address = myServer.accept()
+    if connection is myPeerServer:
+      newConnection, clientToRelayServer_address = myPeerServer.accept()
       readList.append(newConnection)
-      print("P2P | New connection from", client_address)
+      print("P2P | New connection from", clientToRelayServer_address)
       
-    elif connection is client:
+    elif connection is clientToRelayServer:
       data = connection.recv(4096)
       if not data:
         print("RELAY | Server closed.")
-        readList.remove(client)
+        readList.remove(clientToRelayServer)
       else:
         # Naively assume the data is an address tuple
         host, port = data.decode().split("|")
